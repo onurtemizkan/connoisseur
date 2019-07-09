2a0314bde96518e94d964dd4f182c770e602f0d4
Onur Temizkan <onurtemizkan@gmail.com>
2019-07-09 14:15:18 +0300

    Make review updates.

--- test/connoisseur.spec.js
+++ test/connoisseur.spec.js
@@ -11,27 +11,27 @@ const connoisseur = require(path.join(__dirname, "..", "lib", "connoisseur.js"))
 
 describe("lib", () => {
     it("should return an empty array for an empty string", () => {
-        expect(connoisseur("", "STDIN", { commentPrefix: " → " })).to.be.empty();
+        expect(connoisseur("", "STDIN", { checkPrefix: "→" })).to.be.empty();
     });
 
     it("should return an empty array for a simple line comment", () => {
-        expect(connoisseur("// simple comment", "STDIN", { commentPrefix: " → " })).to.be.empty();
+        expect(connoisseur("// simple comment", "STDIN", { checkPrefix: "→" })).to.be.empty();
     });
 
     it("should return an empty array for a simple inline comment", () => {
-        expect(connoisseur("/* simple comment */", "STDIN", { commentPrefix: " → " })).to.be.empty();
+        expect(connoisseur("/* simple comment */", "STDIN", { checkPrefix: "→" })).to.be.empty();
     });
 
     it("should return an empty array for valid assertions with line comments", () => {
-        expect(connoisseur("3 * 5  // → 15", "STDIN", { commentPrefix: " → " })).to.be.empty();
+        expect(connoisseur("3 * 5  // → 15", "STDIN", { checkPrefix: "→" })).to.be.empty();
     });
 
     it("should return an empty array for valid assertions with inline comments", () => {
-        expect(connoisseur("3 * 5  /* → 15 */", "STDIN", { commentPrefix: " → " })).to.be.empty();
+        expect(connoisseur("3 * 5  /* → 15 */", "STDIN", { checkPrefix: "→" })).to.be.empty();
     });
 
     it("should return an array with assertion error for invalid assertions with line comments", () => {
-        const result = connoisseur("3 * 5  // → 12", "STDIN", { commentPrefix: " → " });
+        const result = connoisseur("3 * 5  // → 12", "STDIN", { checkPrefix: "→" });
         expect(result).to.not.be.empty();
         expect(result[0]).to.be.eql({
             actual: "15",
@@ -48,7 +48,7 @@ describe("lib", () => {
     });
 
     it("should return an array with assertion error for invalid assertions with inline comments", () => {
-        const result = connoisseur("3 * 5  /* → 12 */", "STDIN", { commentPrefix: " → " });
+        const result = connoisseur("3 * 5  /* → 12 */", "STDIN", { checkPrefix: "→" });
         expect(result).to.not.be.empty();
         expect(result[0]).to.be.eql({
             actual: "15",
@@ -65,7 +65,7 @@ describe("lib", () => {
     });
 
     it("should return an array with the exception for code throwing exceptions", () => {
-        const result = connoisseur("hello();  // → 12", "STDIN", { commentPrefix: " → " });
+        const result = connoisseur("hello();  // → 12", "STDIN", { checkPrefix: "→" });
         expect(result).to.not.be.empty();
         expect(result[0]).to.be.eql({
             error: new Error("ReferenceError: hello is not defined"),
